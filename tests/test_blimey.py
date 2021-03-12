3e23b049b4ee8e69325977726f3b11afef690a52
mignonnesaurus <eli@mignonnesaurus.com>
2021-03-12 16:53:58 +0100

    Format test file

--- tests/test_blimey.py
+++ tests/test_blimey.py
@@ -1,8 +1,10 @@
 import pytest
 from rock_paper_scissors import Blimey
 
+
 def test_two_times_two_equals_four():
-    assert 2*2 == 4
+    assert 2 * 2 == 4
+
 
 class TestBlimey:
     def test_blimey_increases_by_9000(self):
